08b04f3c684e3d649cfb9dbf0cf4398654d31f8b
Sarah Ma <sarahjma4@gmail.com>
2019-11-28 18:47:48 +0100

    back end for mister cocktail

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'cocktails', to: "cocktails#index"
+  # get 'cocktails/new', to: "cocktails#new", as: :new_cocktail
+  # post 'cocktails', to: "cocktails#create"
+  # get 'cocktails/:id', to: "cocktails#show", as: :cocktail
+  resources :cocktails, only: [:index, :new, :create, :show] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Cocktail.destroy_all
+Ingredient.destroy_all
+# Dose.destroy_all
+
+Cocktail.create(name: "Mojito")
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "olives")
+Ingredient.create(name: "oranges")
+Ingredient.create(name: "grapes")
+Ingredient.create(name: "eggs")
+Ingredient.create(name: "celery")
+Ingredient.create(name: "irish cream")
+
+puts "#{Cocktail.count}Cocktail!"
+puts "#{Ingredient.count}Ingredient!"
