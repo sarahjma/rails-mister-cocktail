f1b598a270f12c377b595e7b5278a32564f40ea9
Sarah Ma <sarahjma4@gmail.com>
2019-12-01 16:24:50 +0100

    mister cocktail continued

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # get 'cocktails/new', to: "cocktails#new", as: :new_cocktail
   # post 'cocktails', to: "cocktails#create"
   # get 'cocktails/:id', to: "cocktails#show", as: :cocktail
-  resources :cocktails, only: [:index, :new, :create, :show] do
+  resources :cocktails, only: [:index, :new, :create, :show, :destroy] do
     resources :doses, only: [:create]
   end
   resources :doses, only: [:destroy]
